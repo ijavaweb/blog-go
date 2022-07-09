160f5edf4927a151075712cdae11c31686cdc7af
LoveYuBoBo <956410141@qq.com>
2022-07-10 04:34:48 +0800

    second commit

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -16,11 +16,11 @@ var (
 func init() {
 	InfoLogger=logrus.New()
 	ErrorLogger = logrus.New()
-	errFile, err := os.OpenFile("E:\\project\\blog-go\\log\\errors.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	errFile, err := os.OpenFile("../log/error.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("Failed to open error log file: ", err)
 	}
-	file, err := os.OpenFile("E:\\project\\blog-go\\log\\info.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile("../log/info.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalln("Failed to open error log file: ", err)
 	}
